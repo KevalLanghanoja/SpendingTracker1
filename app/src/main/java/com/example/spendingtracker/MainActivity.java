e86e2ab7c5696397d906ad658db556532180a0a1
KevalLanghanoja <46380983+KevalLanghanoja@users.noreply.github.com>
2020-02-23 10:38:36 +0530

    Login and Signup is Complate

---
+++ app/src/main/java/com/example/spendingtracker/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.spendingtracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.spendingtracker.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView textView;
+    EditText etLEmail,etLPassword;
+    Button btnLLogin;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database=FirebaseDatabase.getInstance();
+        users=database.getReference("Users");
+
+        textView=findViewById(R.id.tvLSignup);
+        etLEmail=findViewById(R.id.etLEmail);
+        etLPassword=findViewById(R.id.etLPassword);
+        btnLLogin=findViewById(R.id.btnLLogin);
+        textView.setOnClickListener(MainActivity.this);
+        btnLLogin.setOnClickListener(MainActivity.this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvLSignup:
+                startActivity(new Intent(MainActivity.this, Registration.class));
+                MainActivity.this.finish();
+                break;
+            case R.id.btnLLogin:
+                login(etLEmail.getText().toString(),etLPassword.getText().toString());
+                break;
+        }
+    }
+
+    private void login(final String username, final String password) {
+        users.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.child(username).exists())
+                {
+                    if (!username.isEmpty())
+                    {
+                        User login=dataSnapshot.child(username).getValue(User.class);
+                        if (login.getPassword().equals(password)) {
+                            Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+                            SharedPreferences sp=getSharedPreferences("uName",MODE_PRIVATE);
+                            SharedPreferences.Editor spedit = sp.edit();
+                            spedit.putString("uname",etLEmail.getText().toString());
+                            spedit.commit();
+//                            MainActivity.this.finish();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Please Fill All Field", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "User is not Registerd", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/spendingtracker/Registration.java
@@ -0,0 +1,98 @@
+package com.example.spendingtracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.spendingtracker.Model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class Registration extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvRLogin;
+    EditText etRUsername, etRPassword, etREmail, etRConfirmPassword;
+    Button btnRSignup;
+
+    FirebaseDatabase database;
+    DatabaseReference users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        database=FirebaseDatabase.getInstance();
+        users=database.getReference("Users");
+        tvRLogin = findViewById(R.id.tvRLogin);
+        etRUsername = findViewById(R.id.etRUsername);
+        etREmail = findViewById(R.id.etREmail);
+        etRPassword = findViewById(R.id.etRPassword);
+        etRConfirmPassword = findViewById(R.id.etRConfirmPassword);
+        btnRSignup = findViewById(R.id.btnRSignup);
+        tvRLogin.setOnClickListener(Registration.this);
+        btnRSignup.setOnClickListener(Registration.this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvRLogin:
+                startActivity(new Intent(Registration.this, MainActivity.class));
+                Registration.this.finish();
+                break;
+            case R.id.btnRSignup:
+
+               if(etRUsername.getText().toString().isEmpty() || etRPassword.getText().toString().length()<6 || etREmail.getText().toString().length()<5)
+                {
+                    if(etRUsername.getText().toString().length()<5)
+                    {
+                        Toast.makeText(this, "Username must me 5 letter", Toast.LENGTH_SHORT).show();
+                    }
+                    else if(etREmail.getText().toString().length()<5){
+                        Toast.makeText(this, "Enter valid Email Id", Toast.LENGTH_SHORT).show();   
+                    }
+                    else if(etRPassword.getText().toString().length()<6) {
+                        Toast.makeText(this, "password length is minimum 6 Digit", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    if(etRPassword.getText().toString().equals(etRConfirmPassword.getText().toString())) {
+
+                    final User user= new User(etRUsername.getText().toString(),etRPassword.getText().toString(),etREmail.getText().toString());
+                    users.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if(dataSnapshot.child(user.getUsername()).exists()) {
+                                Toast.makeText(Registration.this, "Already Exist", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                users.child(user.getUsername()).setValue(user);
+                                Toast.makeText(Registration.this, "Successfully Register", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(Registration.this,MainActivity.class));
+                                Registration.this.finish();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {}
+                    });
+                }
+                    else {
+                        Toast.makeText(this, "Password and Confiram Password is not Match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                break;
+
+        }
+    }
+}
