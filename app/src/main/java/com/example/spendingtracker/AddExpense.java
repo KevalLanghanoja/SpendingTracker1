538eabe27211293f3b9012776a916ba91bed17e4
KevalLanghanoja <46380983+KevalLanghanoja@users.noreply.github.com>
2020-03-30 14:59:32 +0530

    alert Dialogbox is Complete

--- app/src/main/java/com/example/spendingtracker/AddExpense.java
+++ app/src/main/java/com/example/spendingtracker/AddExpense.java
@@ -4,11 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -19,15 +26,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class AddExpense extends AppCompatActivity {
+public class AddExpense extends AppCompatActivity implements View.OnClickListener {
     ListView friendList;
+    AlertDialog myDialog;
     DatabaseReference reff;
     ArrayList<String> friendListArray=new ArrayList<String>();
     Spinner spinner;
+    TextView tvPaid;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expense);
+        tvPaid=findViewById(R.id.tvPaid);
+        tvPaid.setOnClickListener(this);
         SharedPreferences sp = getSharedPreferences("uName", MODE_PRIVATE);
         String gName=sp.getString("uname", "Not Found");
         reff= FirebaseDatabase.getInstance().getReference().child("Users").child(gName).child("friends");
@@ -64,7 +77,43 @@ public class AddExpense extends AppCompatActivity {
             }
         });
 
-       
+    }
 
+    String[] mStringArray = new String[friendListArray.size()];
+
+    public void showAlert(){
+        AlertDialog.Builder myBilder=new AlertDialog.Builder(AddExpense.this);
+        final ArrayList<String> selectedItems=new ArrayList();
+        mStringArray = friendListArray.toArray(mStringArray);
+        myBilder.setTitle("Paid Betweeen").setMultiChoiceItems(mStringArray,null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int position, boolean isChecked) {
+                if(isChecked){
+                    if(!selectedItems.contains(position)){
+                        selectedItems.add(mStringArray[position]);
+                    }
+                }
+                else {
+                    selectedItems.remove(mStringArray[position]);
+                }
+            }
+        });
+        myBilder.setPositiveButton("Selected", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                StringBuilder sb=new StringBuilder();
+                for (Object i:selectedItems){
+                    sb.append(i.toString()+"\n");
+                }
+                Toast.makeText(AddExpense.this, sb.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        myDialog=myBilder.create();
+        myDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        showAlert();
     }
 }
